4354ac73bdb9d7a3c37ad1a3c3ba532c72b2b3ff
Jimb Esser <wasteland@gmail.com>
2026-04-20 13:08:28 -0700

    Launch Wavedash from Itch; fix Itch build

--- build/config.project.js
+++ build/config.project.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
 
-  config.extra_index = [{
+  config.extra_index.push({
     name: 'itch',
     defines: {
       ...config.default_defines,
@@ -8,15 +8,15 @@ module.exports = function (config) {
     },
     zip: true,
     wrapper: 'itchlaunch.html',
-  }];
+  });
   config.extra_client_html = ['client/itchlaunch.html'];
 
-  config.extra_index = [{
+  config.extra_index.push({
     name: 'wavedash',
     defines: {
       ...config.default_defines,
       PLATFORM: 'wavedash',
     },
     zip: true,
-  }];
+  });
 };
